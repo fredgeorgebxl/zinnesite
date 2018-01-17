d7afffb8c47f2a7259abcafa56179d90fbc59e50
fredgeorgebxl <freddgeorge@yahoo.com>
2018-01-17 10:25:41 +0100

    debug pour mise online

--- src/AppBundle/Form/VideoType.php
+++ src/AppBundle/Form/VideoType.php
@@ -10,8 +10,8 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 class VideoType extends AbstractType{
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('title', TextType::class, array('label' => 'video.title', 'translation_domain' => 'App'))
-            ->add('link', TextType::class, array('label' => 'video.link', 'translation_domain' => 'App'))
-            ->add('save', SubmitType::class, array('label' => 'video.save', 'translation_domain' => 'App'));
+            ->add('title', TextType::class, array('label' => 'videos.title', 'translation_domain' => 'App'))
+            ->add('link', TextType::class, array('label' => 'videos.link', 'translation_domain' => 'App'))
+            ->add('save', SubmitType::class, array('label' => 'videos.save', 'translation_domain' => 'App'));
     }
 }
