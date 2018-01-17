376271f38f6efb26c448cf8e7239b4ba7a57ea5a
fredgeorgebxl <freddgeorge@yahoo.com>
2018-01-17 12:04:07 +0100

    cache clear button added

--- src/AppBundle/Controller/Admin/DefaultController.php
+++ src/AppBundle/Controller/Admin/DefaultController.php
@@ -4,6 +4,9 @@ namespace AppBundle\Controller\Admin;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Output\BufferedOutput;
 
 /**
  * @Route("/admin")
@@ -19,5 +22,24 @@ class DefaultController extends Controller
         $homepage_slideshow = $entityManager->getRepository(\AppBundle\Entity\Gallery::class)->findOneBy(['homeslide' => 1]);
         
         return $this->render('admin/default/index.html.twig', ['slideshow' => $homepage_slideshow]);
+    }
+    
+    /**
+     * @Route("/clearcache", name="clear_cache")
+     */
+    public function clearCache($env = 'prod', $debug = true)
+    {
+        $kernel = new \AppKernel($env, $debug);
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
+
+        $input = new ArrayInput([
+            'command' => 'cache:clear'
+        ]);
+
+        $output = new BufferedOutput();
+        $application->run($input, $output);
+        
+        return $this->redirectToRoute('admin_home');
     }
 }
